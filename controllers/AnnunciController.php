fe5d1839befc6b0b69ea62dbcda36f405e07512a
umpbrut <mattia.govoni.07@gmail.com>
2026-03-31 09:36:51 +0200

    modificati controller e model annunci

--- controllers/AnnunciController.php
+++ controllers/AnnunciController.php
@@ -17,5 +17,25 @@ class AnnunciController{
         include 'views/template.php';
     }
 
-   
+    public function create(){
+        $libri=$this->model->selectTitoli();
+        $view='views/annunci_form_create.php';
+        include 'views/template.php';
+    }
+
+    public function store(){
+        $prezzo = trim($_POST['prezzo']);
+        $data = trim($_POST['data']);
+        $ora = trim($_POST['ora']);
+        $luogo = trim($_POST['luogo']);
+        $id_creatore = trim($_SESSION['id_utente']);
+        $condizioni = trim($_POST['condizioni']);
+        $id_libro = trim($_POST['id_libro']);
+
+        $param=[$prezzo, $data, $ora, $luogo, $id_creatore, $id_libro, $condizioni];
+        $this->model->insertRecord($param);
+
+        header('location:index.php');
+        exit;
+    }
 }
