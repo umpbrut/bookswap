5f954233d1f5e783496615a01d3ca6c2554e03a6
breda.289 <breda.7851@isit100.fe.it>
2026-05-09 08:50:51 +0200

    Allineamento del progetto

---
+++ BOOKSWAP/src/config/dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+defined('APP') or die('Accesso Negato');
+const HOST = 'lab.isit100.fe.it';
+const DBNAME = 'info5n2526__team5';
+const USERNAME = 'info5n2526';
+const PASSWORD = 'Desmond!=5';
+const CHARSET = 'utf8mb4'; //accetta fino a 4 byte per carattere
